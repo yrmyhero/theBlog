696142ad5e6cb1e2b475bdd5779adb400f1e227e
qqq_X_3 <1067487254@qq.com>
2026-07-25 00:44:21 +0800

    pydantic准备中

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,6 +4,13 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "mysql+asyncmy://myuser:mypassword@db:3306/mydb")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, 
+                             echo=True,
+                             pool_size=20,
+                             max_overflow=10,
+                             pool_recycle=3600,
+                             pool_pre_ping=True
+                             )
+
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
